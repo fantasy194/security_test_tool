2668ff30d5068487c5b91a66b1ec44aa4a1c52bb
fantasy194 <867275816@qq.com>
2023-07-24 15:46:13 +0800

    Update security_test_tool.js
    Support outside player

--- security_test_tool.js
+++ security_test_tool.js
@@ -1,23 +1,41 @@
-let orgFun = window.globalProvideData;
-window.globalProvideData = new Proxy(orgFun, {
-    apply: (t, thisArg, args) => {
+/**
+ * @Created by jiangxinchen on 2023/7/24
+ */
+
+const playerWindow = (window.globalProvideData && window) || document.querySelector("#main_iframe")?.contentDocument.querySelector("#jca_course_player")?.contentWindow.window
+const playerDocument = playerWindow?.document
+
+let orgFun = playerWindow?.globalProvideData;
+
+if (!orgFun) {
+    console.error("Hook failed!")
+}
+
+playerWindow.globalProvideData = new Proxy(orgFun, {
+    apply: (t, thisArg, [dataMode, data]) => {
         try {
-            refreshAnswers(args[1])
+            if ("slide" === dataMode) {
+                refreshSlideAnswers(data)
+            }
         } catch (e) {
             console.error(e)
         }
-        return t(args[0], args[1])
+        return t(dataMode, data)
     }
 })
 
-refreshAnswers = (e) => {
+refreshSlideAnswers = (e) => {
     let correct = [];
     const regex = /\"(\w+)\.(\w+)\.\$OnStage\"/gm;
     while ((result = regex.exec(e)) != null) {
         correct.push(result[2]);
     }
     for (let id of correct) {
-        setTimeout(() => document.querySelector(".slide-object[data-model-id='" + id + "']")?.style?.setProperty("background-color", "green"), 100)
+        console.log("Correct: " + id)
+        setTimeout(() => {
+            let element = playerDocument.querySelector(".slide-object[data-model-id='" + id + "']");
+            element?.style?.setProperty("background-color", "green")
+        }, 100)
     }
 }
-document.querySelector("#frame").insertAdjacentHTML('afterbegin', '<button style="position:absolute;top:5px;right:5px;z-index:1000" onclick="DS.pubSub.trigger(DS.events.presentation.ON_OBJECT_EVENT, \'next_pressed\')">JUMP!!!</button>');
+playerDocument.querySelector("#frame")?.insertAdjacentHTML('afterbegin', '<button style="position:absolute;top:5px;right:5px;z-index:1000" onclick="DS.pubSub.trigger(DS.events.presentation.ON_OBJECT_EVENT, \'next_pressed\')">JUMP!!!</button>');
